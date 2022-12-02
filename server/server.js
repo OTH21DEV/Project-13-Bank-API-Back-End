70491b26c0a189af1ac9c6dcc3f6e243541fb101
Oxana Theis <otheis@protonmail.com>
2022-12-02 11:16:29 +0100

    test

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000
 
 
 /**test */
-const host = '127.0.0.1';
+//const host = '127.0.0.1';
 const path = require ('path')
 //
 
@@ -52,6 +52,6 @@ app.get('/', (req, res, next) => {
   res.send('Hello from my Express server v2!')
 })
 
-app.listen(port,host, () => {
+app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
 })
